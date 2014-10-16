dc54491eba226daa9d8bf70b3a41dccd47240244
Fabien Hermenier <hermenierfabien@gmail.com>
2014-10-16 23:40:04 +0200

    more on datastore

--- src/main/java/org/btrplace/playd/model/UseCase.java
+++ src/main/java/org/btrplace/playd/model/UseCase.java
@@ -9,12 +9,15 @@ public class UseCase {
 
     private String description;
 
-    private String instance;
+    private String model;
 
-    public UseCase(String ti, String desc, String ins) {
+    private String script;
+
+    public UseCase(String ti, String desc, String mo, String scr) {
         title = ti;
-        description = description;
-        instance = ins;
+        description = desc;
+        model = mo;
+        script = scr;
     }
 
     public String title() {
@@ -25,7 +28,21 @@ public class UseCase {
         return description;
     }
 
-    public String instance() {
-        return instance;
+    public String toJson() {
+        StringBuilder b = new StringBuilder();
+        b.append("{");
+        b.append("\"title\": \"").append(title()).append("\",");
+        b.append("\"description\": \"").append(description()).append("\",");
+        b.append("\"model\": \"").append(model()).append("\",");
+        b.append("\"script\": \"").append(script()).append("\"");
+        return b.append("}").toString();
+    }
+
+    public String model() {
+        return model;
+    }
+
+    public String script() {
+        return script;
     }
 }
--- src/main/java/org/btrplace/playd/service/Store.java
+++ src/main/java/org/btrplace/playd/service/Store.java
@@ -4,8 +4,10 @@ import org.btrplace.playd.model.UseCase;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -20,11 +22,15 @@ public class Store {
     public Store() {
         useCases = new ArrayList<>();
         //Mock
-        useCases.add(new UseCase("server decommissioning", "the description", "foo"));
-        useCases.add(new UseCase("foo", "foo desc", "foo instance"));
-        useCases.add(new UseCase("bar", "bar desc", "bar instance"));
+        useCases.add(new UseCase("decommissioning", "deco", "", "namespace sandbox;\\n\\n" +
+                "VM[1..8] : myVMs;\\n\\n" +
+                ">>spread({VM1, VM5});\\n" +
+                "ban(VM3, @N1);\\n" +
+                ">>maxOnline(@N[1..8], 5);"));
     }
+
     @GET
+    @Path("/")
     @Produces(MediaType.APPLICATION_JSON)
     public List<String> list() {
         List<String> titles = new ArrayList<>(useCases.size());
@@ -32,5 +38,17 @@ public class Store {
             titles.add(uc.title());
         }
         return titles;
+    }
+
+    @GET
+    @Path("/{id}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response get(@PathParam("id") String id) {
+        for (UseCase uc : useCases) {
+            if (uc.title().equals(id)) {
+                return Response.ok(uc.toJson()).build();
+            }
+        }
+        return Response.status(Response.Status.NOT_FOUND).build();
     }
 }
