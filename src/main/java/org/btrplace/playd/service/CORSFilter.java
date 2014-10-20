6d7e232ef4ac115fe24cb83a1e6ff75a75729ea4
Fabien Hermenier <hermenierfabien@gmail.com>
2014-10-20 12:34:30 +0200

    fix cors ?

--- src/main/java/org/btrplace/playd/service/CORSFilter.java
+++ src/main/java/org/btrplace/playd/service/CORSFilter.java
@@ -13,13 +13,15 @@ import javax.ws.rs.ext.Provider;
 public class CORSFilter implements ContainerResponseFilter {
 
     @Override
-    public ContainerResponse filter(final ContainerRequest requestContext,
-                       final ContainerResponse cres) {
+    public ContainerResponse filter(final ContainerRequest requestContext, final ContainerResponse cres) {
         cres.getHttpHeaders().add("Access-Control-Allow-Origin", "*");
-        cres.getHttpHeaders().add("Access-Control-Allow-Headers", "origin, content-type, accept, authorization");
-        cres.getHttpHeaders().add("Access-Control-Allow-Credentials", "true");
-        cres.getHttpHeaders().add("Access-Control-Allow-Methods", "POST GET OPTIONS");
-        cres.getHttpHeaders().add("Access-Control-Max-Age", "1209600");
+        cres.getHttpHeaders().add("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
+
+        String reqHead = requestContext.getHeaderValue("Access-Control-Request-Headers");
+        if(null != reqHead && !reqHead.equals("")){
+            cres.getHttpHeaders().add("Access-Control-Allow-Headers", reqHead);
+        }
+
         return cres;
     }
 
