eeb83e86e23643424a8b91254cacde3c3c4bd94d
Fabien Hermenier <hermenierfabien@gmail.com>
2014-10-20 13:51:30 +0200

    fix cors ?

--- src/main/java/org/btrplace/playd/service/CORSFilter.java
+++ src/main/java/org/btrplace/playd/service/CORSFilter.java
@@ -4,6 +4,7 @@ import com.sun.jersey.spi.container.ContainerRequest;
 import com.sun.jersey.spi.container.ContainerResponse;
 import com.sun.jersey.spi.container.ContainerResponseFilter;
 
+import javax.ws.rs.core.Response;
 import javax.ws.rs.ext.Provider;
 
 /**
@@ -13,9 +14,9 @@ import javax.ws.rs.ext.Provider;
 public class CORSFilter implements ContainerResponseFilter {
 
     @Override
-    public ContainerResponse filter(final ContainerRequest requestContext, final ContainerResponse cres) {
-        cres.getHttpHeaders().add("Access-Control-Allow-Origin", "*");
-        cres.getHttpHeaders().add("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
+    public ContainerResponse filter(final ContainerRequest req, final ContainerResponse cres) {
+        /*cres.getHttpHeaders().add("Access-Control-Allow-Origin", "*");
+        cres.getHttpHeaders().add("Access-Control-Allow-Methods", "GET, POST, PUT, OPTIONS");
         cres.getHttpHeaders().add("Access-Control-Max-Age", "1209600");
 
         String reqHead = requestContext.getHeaderValue("Access-Control-Request-Headers");
@@ -23,6 +24,29 @@ public class CORSFilter implements ContainerResponseFilter {
             cres.getHttpHeaders().add("Access-Control-Allow-Headers", reqHead);
         }
 
+        return cres;*/
+
+        Response.ResponseBuilder crunchifyResponseBuilder = Response.fromResponse(cres.getResponse());
+
+        // *(allow from all servers) OR http://crunchify.com/ OR http://example.com/
+        crunchifyResponseBuilder.header("Access-Control-Allow-Origin", "http://crunchify.com/")
+
+                // As a part of the response to a request, which HTTP methods can be used during the actual request.
+                .header("Access-Control-Allow-Methods", "API, GET, POST, PUT, UPDATE, OPTIONS")
+
+                        // How long the results of a request can be cached in a result cache.
+                .header("Access-Control-Max-Age", "151200")
+
+                        // As part of the response to a request, which HTTP headers can be used during the actual request.
+                .header("Access-Control-Allow-Headers", "x-requested-with,Content-Type");
+
+        String crunchifyRequestHeader = req.getHeaderValue("Access-Control-Request-Headers");
+
+        if (null != crunchifyRequestHeader && !crunchifyRequestHeader.equals(null)) {
+            crunchifyResponseBuilder.header("Access-Control-Allow-Headers", crunchifyRequestHeader);
+        }
+
+        cres.setResponse(crunchifyResponseBuilder.build());
         return cres;
     }
 
