fb13fcae0141b748760706f2281354124f983607
Fabien Hermenier <hermenierfabien@gmail.com>
2014-10-27 18:10:48 +0100

    Revert "support preserve"
    This reverts commit 458e49e26c3c34dfe66b69462c263d9a0758892e.

--- src/main/java/org/btrplace/playd/service/Solver.java
+++ src/main/java/org/btrplace/playd/service/Solver.java
@@ -115,7 +115,6 @@ public class Solver {
         c.add(new SeqBuilder());
         c.add(new MaxOnlineBuilder());
         c.add(new NoDelayBuilder());
-        c.add(new PreserveBuilder());
         return c;
     }
 
