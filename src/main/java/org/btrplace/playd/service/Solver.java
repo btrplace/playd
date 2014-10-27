d3a161235dad380db7784d12bba1fd5c8fe8e211
Fabien Hermenier <hermenierfabien@gmail.com>
2014-10-27 18:03:28 +0100

    durations integration

--- src/main/java/org/btrplace/playd/service/Solver.java
+++ src/main/java/org/btrplace/playd/service/Solver.java
@@ -121,7 +121,7 @@ public class Solver {
 
     private static DurationEvaluators makeDurations() {
         DurationEvaluators dev = DurationEvaluators.newBundle();
-        dev.register(MigrateVM.class, new LinearToAResourceActionDuration<>("mem", 1.0, 1));
+        dev.register(MigrateVM.class, new LinearToAResourceActionDuration<>("mem", 1.0, 0));
         dev.register(BootNode.class, new ConstantActionDuration<>(2));
         dev.register(ShutdownNode.class, new ConstantActionDuration<>(2));
         return dev;
